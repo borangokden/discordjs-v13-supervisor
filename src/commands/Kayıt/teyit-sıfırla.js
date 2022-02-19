4434c9f98ba2447b3b68a960894533b2cd0de38e
BoranGkdn <90551725+BoranGkdn@users.noreply.github.com>
2022-02-19 11:37:23 +0300

    Update teyit-sıfırla.js

--- src/commands/Kayıt/teyit-sıfırla.js
+++ src/commands/Kayıt/teyit-sıfırla.js
@@ -7,7 +7,7 @@ moment.locale("tr");
 const ms = require("ms")
 module.exports = {
   name: "kayıt-sıfırla",
-  aliases: ["kayıt-sifirla", "teyit-sıfırla"],
+  aliases: ["kayıt-sifirla", "teyit-sıfırla", "kayıtsıfırla"],
   execute: async (client, message, args, embed, author, channel, guild) => {
     
     if (!message.member.roles.cache.has(config.bot.owner) && !message.member.roles.cache.has(config.bot.owner) && !message.member.permissions.has(8)) return message.reply({ embeds: [embed.setDescription("Komutu kullanmak için geçerli yetkin olmalı!")] }).catch((err) => console.log(err), client.tick(message)).then((e) => setTimeout(() => { e.delete(); }, 10000));
@@ -15,17 +15,17 @@ module.exports = {
     var member = message.mentions.users.first() || guild.members.cache.get(args[0]);
 
 if (!member) {
-let erkek = db.delete(`erkek_${member}`) || [];
-let kadın = db.delete(`kadın_${member}`) || [];
-let toplam = db.delete(`toplam_${member}`) || [];
+let erkek = db.delete(`erkek_${author.id}`) || [];
+let kadın = db.delete(`kadın_${author.id}`) || [];
+let toplam = db.delete(`toplam_${author.id}`) || [];
 message.reply({ embeds: [embed.setDescription(`Kayıt verilerin silindi.`)] });
 }
   
 if(member) {
-let erkek = db.delete(`erkek_${member}`) || [];
-let kadın = db.delete(`kadın_${member}`) || [];
-let toplam = db.delete(`toplam_${member}`) || [];
-message.reply({ embeds: [embed.setDescription(`${member} kullanıcısının kayıt verileri başarıyla silindi.`)] });
+let erkek = db.delete(`erkek_${member.id}`) || [];
+let kadın = db.delete(`kadın_${member.id}`) || [];
+let toplam = db.delete(`toplam_${member.id}`) || [];
+message.reply({ embeds: [embed.setDescription(`${member} kayıt verileri silindi.`)] });
 
 };
   
