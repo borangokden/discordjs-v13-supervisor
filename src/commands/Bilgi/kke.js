daad3e494a963170acee506793c82bf50ce33d42
BoranGkdn <90551725+BoranGkdn@users.noreply.github.com>
2022-02-21 10:59:00 +0300

    Update kke.js

--- src/commands/Bilgi/kke.js
+++ src/commands/Bilgi/kke.js
@@ -8,10 +8,10 @@ module.exports = {
         var member = message.mentions.users.first() || guild.members.cache.get(args[0])
         
         if (!member) return message.reply({ embeds: [embed.setDescription("Geçerli bir kullanıcı belirtmelisin!")] }).catch((err) => console.log(err), client.tick(message)).then((e) => setTimeout(() => { e.delete(); }, 10000));
-        let kav = db.get(`kav_${member.id}`);
-        if (!kav) return message.reply({ embeds: [embed.setDescription("Bu kullanıcının veri tabanında kayıt verisi bulunmamakta!")] }).catch((err) => console.log(err), client.ytick(message)).then((e) => setTimeout(() => { e.delete(); }, 10000));
+        let kke = db.get(`kke_${member.id}`);
+        if (!kke) return message.reply({ embeds: [embed.setDescription("Bu kullanıcının veri tabanında kayıt verisi bulunmamakta!")] }).catch((err) => console.log(err), client.ytick(message)).then((e) => setTimeout(() => { e.delete(); }, 10000));
         message.reply({ embeds: [embed.setDescription(`${member} kullanıcısının veri tabanındaki kayıt görevlisi:
         
-        ${kav.join("\n")}`)] });
+        ${kke.join("\n")}`)] });
     }
 }
