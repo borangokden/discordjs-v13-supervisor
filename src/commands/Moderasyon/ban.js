fc27f388e770a8ec4591a84afcfc6eb4ff3a33cf
BoranGkdn <90551725+BoranGkdn@users.noreply.github.com>
2022-04-13 20:18:02 +0300

    Update ban.js

--- src/commands/Moderasyon/ban.js
+++ src/commands/Moderasyon/ban.js
@@ -16,7 +16,7 @@ module.exports = {
     if (!user) return message.reply({ embeds: [embed.setDescription('Öncelikle banlanacak kullanıcıyı belirtmelisin.')] }).catch((err) => console.log(err), client.tick(message)).then((e) => setTimeout(() => { e.delete(); }, 10000));
     if (reason.length < 1) return message.reply({ embeds: [embed.setDescription('Öncelikle geçerli bir sebep belirtmelisin.')] }).catch((err) => console.log(err), client.tick(message)).then((e) => setTimeout(() => { e.delete(); }, 10000));
     if (config.penals.ban.limit > 0 && limit.has(author.id) && limit.get(author.id) == config.penals.ban.limit) return message.reply({ embeds: [embed.setDescription("Saatlik ban sınırına ulaştın!")] }).catch((err) => console.log(err), client.tick(message)).then((e) => setTimeout(() => { e.delete(); }, 10000));
-    if (!message.member.permissions.has("ADMINISTRATOR") && member && member.roles.highest.position >= message.member.roles.highest.position) return message.reply({ embeds: [embed.setDescription("Kendinle aynı yetkide ya da daha yetkili olan birini muteleyemezsin!")] })
+    if (!message.member.permissions.has("ADMINISTRATOR") && member && member.roles.highest.position >= message.member.roles.highest.position) return message.reply({ embeds: [embed.setDescription("Kendinle aynı yetkide ya da daha yetkili olan birini yasaklayamazsın!")] })
     guild.members.ban(user, { reason: reason });
     message.reply({ embeds: [embed.setDescription(`**${user.user.tag}** - **(${user.id})** kullanıcısı ${author} tarafından **"${reason}"** sebebiyle sunucudan yasaklandı. \`(Ceza ID: #${db.fetch(`ceza_${guild.id}`)})\``)] }).catch((err) => console.log(err), client.ytick(message))
     db.add(`ceza_${guild.id}`, 1)
