a967b2239b2232cc9686b2a4cffcbd5fd21a6a92
BoranGkdn <90551725+BoranGkdn@users.noreply.github.com>
2022-04-15 10:45:04 +0300

    Update comandHandler.js

--- src/events/comandHandler.js
+++ src/events/comandHandler.js
@@ -40,8 +40,8 @@ if (message.author.id === uye) {
     const guild = message.guild
     const embed = new MessageEmbed()        
         .setColor(message.member.displayHexColor)
-        .setAuthor({name: message.member.displayName, iconURL: author.avatarURL({ dynamic: true, size: 2048 })})
-        .setFooter({text: (config.bot.BotFooter) , iconURL: ownerr.avatarURL({ dynamic: true, size: 2048 })})
+        .setAuthor({name: message.member.displayName))
+        .setFooter({text: (config.bot.BotFooter))
     if (cmd) {
         if (cmd.owner && config.bot.owner !== author.id) return;
         if (cmd.guildowner && config.bot.owner !== author.id && guild.owner.id !== author.id) return;
